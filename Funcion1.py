8d4004647e3bb1cf2aa459c56067d4617e3e5e9e
DiegoValdez06 <103142754+DiegoValdez06@users.noreply.github.com>
2022-07-13 18:56:15 -0500

    Creacion de la funcion1

---
+++ Funcion1.py
@@ -0,0 +1,8 @@
+usuario = "@"
+clave = 0
+
+while usuario != "Diego" or clave != 12345:
+    usuario = input("Nombre de usuario: ")
+    clave = int(input("Ingresa la clave: "))
+
+print("Bienvenido Diego!")
